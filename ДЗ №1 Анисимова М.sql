a3b2af6d84053eac60004a0ffe60abe42bfe8841
Mariia25 <91418432+Mariia25@users.noreply.github.com>
2021-09-26 15:46:45 +0300

    Add files via upload

---
+++ ДЗ №1 Анисимова М.sql
@@ -0,0 +1,72 @@
+Задание 1: Вывести name, class по кораблям, выпущенным после 1920
+select name, class 
+from ships
+where launched > 1920
+
+Задание 2: Вывести name, class по кораблям, выпущенным после 1920, но не позднее 1942
+select name, class
+from ships
+where launched > 1920 and launched < 1942
+
+Задание 3: Какое количество кораблей в каждом классе. Вывести количество и class  ---
+
+select class, count (ships)
+from ships
+group by class
+
+
+
+Задание 4: Для классов кораблей, калибр орудий которых не менее 16, укажите класс и страну. (таблица classes)
+
+SELECT class, country
+From classes
+WHERE bore >= 16
+
+Задание 5: Укажите корабли, потопленные в сражениях в Северной Атлантике (таблица Outcomes, North Atlantic). Вывод: ship.
+
+select ship
+from outcomes
+where result = 'sunk'
+and battle = 'North Atlantic'
+
+
+select ship
+from battles
+left join outcomes on battle = name 
+where name = 'North Atlantic' and result = 'sunk'
+
+Задание 6: Вывести название (ship) последнего потопленного корабля
+
+select name
+from ships
+order by launched desc
+limit 1
+
+Задание 7: Вывести название корабля (ship) и класс (class) последнего потопленного корабля
+
+select name, class
+from ships
+order by launched desc
+limit 1
+
+
+Задание 8: Вывести все потопленные корабли, у которых калибр орудий не менее 16, и которые потоплены. Вывод: ship, class
+
+select ship, classes.class
+  from outcomes 
+  join classes 
+  on outcomes.ship = classes.class
+  where result = 'sunk' and bore >= 16
+  
+Задание 9: Вывести все классы кораблей, выпущенные США (таблица classes, country = 'USA'). Вывод: class
+
+select class
+from classes
+where country = 'USA'
+
+Задание 10: Вывести все корабли, выпущенные США (таблица classes & ships, country = 'USA'). Вывод: name, class
+
+select name, classes.class
+from classes join ships 
+on classes.class = ships.name
+Where country = 'USA'
